39b320c6ddaa9e2cab85fba2a0005c9455590274
lipsitasahu <lipsita5521@gmail.com>
2025-11-26 13:57:34 +0530

    Dashboard screen base created

--- app/src/main/java/com/example/speedometer/screen/DashboardScreen.kt
+++ app/src/main/java/com/example/speedometer/screen/DashboardScreen.kt
@@ -1,4 +1,29 @@
 package com.example.speedometer.screen
 
-class DashboardScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.speedometer.screen.components.Speedometer
+
+@Preview
+@Composable
+fun DashboardScreen() {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Speedometer(20, "mph")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Speedometer(10, "km/h")
+        }
+        Speedometer(50, "km/h")
+    }
 }
