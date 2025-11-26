10c89a82bf6cb237e9a615a87cadc7523e7355a5
lipsitasahu <lipsita5521@gmail.com>
2025-11-26 12:12:50 +0530

    Speedometer component created

--- app/src/main/java/com/example/speedometer/screen/components/Speedometer.kt
+++ app/src/main/java/com/example/speedometer/screen/components/Speedometer.kt
@@ -1,4 +1,44 @@
 package com.example.speedometer.screen.components
 
-class Speedometer {
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Speedometer(speed: Int, unit: String) {
+    Box(contentAlignment = Alignment.Center) {
+        Canvas(modifier = Modifier.size(200.dp)) {
+            val sweepAngle = (speed / 240f) * 270f
+            drawArc(
+                color = Color(0xFF7DF9FF),
+                startAngle = 135f,
+                sweepAngle = sweepAngle,
+                useCenter = false,
+                style = Stroke(width = 20f, cap = StrokeCap.Round)
+            )
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("$speed", fontSize = 48.sp, color = Color.White, fontWeight = FontWeight.Bold)
+            Text(unit, fontSize = 16.sp, color = Color(0xFF66C1D6))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SpeedometerPreview() {
+    Speedometer(10,"km/h")
 }
